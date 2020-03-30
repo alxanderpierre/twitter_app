111042beaf747dac317b22370a4063a7131658f1
alxanderpierre <alxanderpierre@users.noreply.github.com>
2020-03-30 12:07:11 -0400

    Users route

--- web_app/routes/twitter_routes.py
+++ web_app/routes/twitter_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, jsonify, request, render_template, flash, redirect
 
 from web_app.twitter_service import twitter_api
 from web_app.models import db, User, Tweet
@@ -7,6 +7,17 @@ twitter_api_client = twitter_api()
 
 twitter_routes = Blueprint("twitter_routes", __name__)
 
+@twitter_routes.route("/users")
+@twitter_routes.route("/users.json")
+def list_users():
+    users = []
+    user_records = User.query.all()
+    for user in user_records:
+        print(user)
+        d = user.__dict__
+        del d["_sa_instance_state"]
+        users.append(d)
+    return jsonify(users)
 
 @twitter_routes.route("/users/<screen_name>")
 def get_user(screen_name=None):
